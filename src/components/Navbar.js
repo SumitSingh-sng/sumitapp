9867383612ede2b8ca573ba7ef459e0072649b4b
Sumit <sngsumit2003@gmail.com>
2024-01-31 10:38:16 +0530

    added new files in the app.

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a class="navbar-brand" href="/">
+    <img src="/public/apple-touch-icon.png" alt="Icon" />
+    </a>
+
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+    </>
+  )
+}
